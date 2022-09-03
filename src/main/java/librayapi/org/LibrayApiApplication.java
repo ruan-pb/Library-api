05ba8a419a591888e8f92ccdff60a9aed2fbdf1e
ruan-pb <ruancruzsoares@hotmail.com>
2022-09-03 15:28:45 -0300

    Testes de integração

---
+++ src/main/java/librayapi/org/LibrayApiApplication.java
@@ -0,0 +1,13 @@
+package librayapi.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrayApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/librayapi/org/LibrayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package librayapi.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
